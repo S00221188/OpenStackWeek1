9f3a2cf5fa6d4254438f0f8dc6460431cfee3f13
MalachyS <s00221188@atu.ie>
2024-01-23 12:15:45 +0000

    task 2 Complete -- toBeGreaterThan added

--- TestingApp1/src/app/basics/compute.spec.ts
+++ TestingApp1/src/app/basics/compute.spec.ts
@@ -5,4 +5,14 @@ describe ('compute', () => {
         const result = compute(-1);
         expect(result).toBe(0);
     })
-})
+
+    it ("should return input+1 if the input is positive", () => {
+        const result = compute(1);
+        expect(result).toBe(2);
+    })
+    //toBeGreaterThan
+    it ("should check if a value is greater than another", () => {
+        const result = compute(1)
+        expect(result).toBeGreaterThan(0);
+    })
+});
